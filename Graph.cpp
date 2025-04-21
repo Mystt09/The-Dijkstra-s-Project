3a942b68e3940fb8c520f243ae277f3bab9102ec
Shukrimuhi <smuhidi1@asu.edu>
2025-04-20 22:54:19 -0700

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -1,1 +1,15 @@
+#include "Graph.h"
+#include <isostream>
 
+Graph::Graph(int vert) {
+    numVert = vert;
+    numEdges = 0;
+
+    adjMatrix = new int*[numVert];
+    for (int i = 0; i < numVert; i++) {
+        adjMatrix[i] = new int[numVert];
+        for (int j = 0; j < numVert; j++) {
+            adjMatrix[i][j] = 0;
+    }
+    }
+}
